36dc443800c36ab948401a40804ef9aceb90702e
Tetardtek <kvnn64@gmail.com>
2023-10-09 18:18:51 +0200

    My frist component!

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,30 @@
+function PokemonCard() {
+return <figure>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
+    <figcaption>Bulbasaur</figcaption>
+</figure>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default PokemonCard;
