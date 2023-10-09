62552f61cc6cb0cda70f5dee29e1a9b72acf39ca
Tetardtek <kvnn64@gmail.com>
2023-10-09 19:04:27 +0200

    Add New Pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -7,15 +7,25 @@ const pokemonList = [
     {
       name: "Mew",
     },
+    {
+        name: "Squirtle",
+        imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      }, 
+      {
+        name: "Charmander",
+        imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
   ];
   
 
- let pokemon = pokemonList[1];
+ let pokemon = pokemonList[3];
   
   function PokemonCard() {
     if (pokemon.imgSrc !== undefined) {
   return <figure>
-      <img src={pokemon.imgSrc} alt={pokemon.name}/>
+      <img className="pkmnPic" src={pokemon.imgSrc} alt={pokemon.name}/>
       <figcaption>{pokemon.name}</figcaption>
   </figure>
     } else {
